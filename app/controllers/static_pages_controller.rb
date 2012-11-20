fb63d6eaa2e38bf45230ac2179e7ac95161f49ba
Caroline Horn <clhorn99@gmail.com>
2012-11-20 13:53:21 -0500

    Dynamic Products number in Admin sidebar; Dynamic page titles

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,34 +1,50 @@
 class StaticPagesController < ApplicationController
+  
+  
   def home
-    @title = "help";
+    @title = "Home";
   end
 
   def buttons
+    @title = "Buttons & Colors";
+    @products = Product.all;
     render :layout => 'admin';
   end
 
   def forms
+    @title = "Forms";
+    @products = Product.all;
     render :layout => 'admin';
   end
 
   def other
+    @title = "Other";
+    @products = Product.all;
     render :layout => 'admin';
   end
   
   def icons
+    @title = "Icons";
+    @products = Product.all;
     render :layout => 'admin';
   end
   
   def general
+    @title = "General Elements";
+    @products = Product.all;
     render :layout => 'admin';
   end
   
   def tables
+    @title = "Tables";
+    @products = Product.all;
     render :layout => 'admin';
   end
   
   def dashboard
-     render :layout => 'admin';
+    @title = "Dashboard";
+    @products = Product.all;
+    render :layout => 'admin';
   end
   
 end
