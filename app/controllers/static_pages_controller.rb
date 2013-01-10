81b2e069a73bae3a56bf6a1faadebcbfddd81672
Caroline Horn <clhorn99@gmail.com>
2013-01-10 11:53:43 -0500

    Updated rails to 3.2.11

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,37 +7,30 @@ class StaticPagesController < ApplicationController
 
   def buttons
     @title = "Buttons & Colors";
-    @products = Product.all;
   end
 
   def forms
     @title = "Forms";
-    @products = Product.all;
   end
 
   def other
     @title = "Other";
-    @products = Product.all;
   end
   
   def icons
     @title = "Icons";
-    @products = Product.all;
   end
   
   def general
     @title = "General Elements";
-    @products = Product.all;
   end
   
   def tables
     @title = "Tables";
-    @products = Product.all;
   end
   
   def dashboard
     @title = "Dashboard";
-    @products = Product.all;
   end
   
 end
