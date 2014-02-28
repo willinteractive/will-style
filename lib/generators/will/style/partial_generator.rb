5512753901f02e9568920b9b9b616a63b68e6130
Dillon Lucente <dillonlucente@gmail.com>
2014-02-28 16:43:00 -0500

    #10 Template Generator.
    rails g will:style:template
    
    No arguments will give you a list of available templates
    One argument will copy a partial to the clipboard
    Two arguments will save partial to a file, nested under ‘app/views/‘.
    If the file exists, it will append it to the end of the file.

---
+++ lib/generators/will/style/partial_generator.rb
@@ -0,0 +1,51 @@
+module Will
+  module Style
+    module Generators
+      class PartialGenerator < Rails::Generators::Base
+        desc "This generates a partial for you. Use with no arguments to get a list of available partials. Use with just a partial name to copy partial content to the clipboard (mac only). Use with a partial name and a file location (e.g. /products/form.html.erb) to save or append partial to a file."
+
+        argument :partial_name, type: :string, default: ""
+        argument :save_path, type: :string, default: ""
+
+        def partial
+          partials = Dir.entries(File.expand_path("../../../partials", __FILE__))
+
+          if partial_name == ""
+            p "Here are your available partials"
+            p "--------------------------------"
+            partials.each do |partial|
+              p partial.gsub(".html.erb", "") unless partial == "." || partial == ".."
+            end
+          else
+            found_partial = false
+
+            partials.each do |partial|
+              if partial.gsub(".html.erb", "").downcase == partial_name.downcase
+                p "Generating Partial #{partial}"    
+                found_partial = true
+
+                file_content = File.read(File.expand_path("../../../partials/#{partial}", __FILE__))
+                if save_path == ""
+                  pbcopy file_content
+                  p "Partial copied to clipboard"
+                else
+                  new_filename = "app/views/#{save_path}.html.erb"
+                  File.open(new_filename, 'a') { |file| file.write(file_content) }
+                end
+              end
+            end
+
+            p "Could not find partial by the name '#{partial_name}'. Use this generator without any arguments to see a list of partials." if found_partial == false
+          end          
+        end
+
+        private
+          def pbcopy(input)
+            str = input.to_s
+            IO.popen('pbcopy', 'w') { |f| f << str }
+            str
+          end
+      end
+    end
+  end
+end
