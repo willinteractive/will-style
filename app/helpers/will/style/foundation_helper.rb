e646890fe9586b51f405ea9820c0aafc66504cba
Dillon Lucente <dillonlucente@gmail.com>
2014-07-15 14:54:06 -0400

    Tooltip helper.

--- app/helpers/will/style/foundation_helper.rb
+++ app/helpers/will/style/foundation_helper.rb
@@ -63,6 +63,42 @@ module WILL
           end
         end
       end
+
+      ##
+      # Generates a tooltip span.
+      # = Params
+      # +text+
+      #
+      # text you would like to display in the span.
+      #
+      # +title+
+      #
+      # tooltip text.
+      #
+      # +position+
+      #
+      # Position of the tooltip.
+      #
+      # <i>Possible values</i>: bottom, top, left, right. 
+      #
+      # <i>Default value</i>: bottom.
+      #
+      # +options+
+      #
+      # Hash of options.
+      #
+      # :class – will add classes to the tooltip. 
+      #
+      # :options - Foundation tooltip data-options. (disable_for_touch:true, show_on:large)
+      #
+      # = Examples
+      #   <%= f_tooltip "Click Here", "This takes you to a new place", "top", class: "round", options: "disable_for_touch:true" %>
+      #
+      def f_tooltip(text="", title="", position="bottom", options={})
+        content_tag(:span, data: { tooltip: true, options: options[:options] }, class: "has-tip tip-#{position} #{options[:class]}", title: title) do
+          text
+        end
+      end
     end
   end
 end
