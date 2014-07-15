bc9420d72693509c3cba1d63165b1911594aca1e
Dillon Lucente <dillonlucente@gmail.com>
2014-07-15 16:49:50 -0400

    Added complex modal helper by using a builder object.

--- app/helpers/will/style/foundation_helper.rb
+++ app/helpers/will/style/foundation_helper.rb
@@ -2,13 +2,16 @@
 # Namespace module.
 #
 module WILL
+
   ##
   # Namespace module.
   #
   module Style
+
     ##
     # Helpers for generating Foundation HTML elements
     module FoundationHelper
+
       ##
       # Generates an alert box. You can either specify alert text
       # or use an ERB Block to fill in HTML content.
@@ -29,9 +32,11 @@ module WILL
       #
       # Hash of options.
       #
+      # :id – element id.
+      #
       # :class – will add classes to the alert. 
       #
-      # :dismissable - whether or not to include the close button. Defaults to true.
+      # :dismissable - whether or not to include the close button. Defaults to false.
       #
       # +block+
       #
@@ -46,7 +51,7 @@ module WILL
       #   <% end %>
       #
       def f_alert(text="", type="info", options={}, &block)
-        if block
+        if block_given?
           content = capture(&block)
 
           options = options.merge(type) if type.is_a?(Hash)
@@ -55,11 +60,11 @@ module WILL
           content = content_tag(:span, text)
         end
 
-        content_tag(:div, data: { alert: true }, class: "alert-box #{type} #{options[:class]}") do
-          if options.has_key?(:dismissable) && options[:dismissable] == false
-            content
-          else
+        content_tag(:div, data: { alert: true }, class: "alert-box #{type} #{options[:class]}", id: options[:id]) do
+          if options.has_key?(:dismissable) && options[:dismissable] == true
             content + link_to("&times;".html_safe, "#", class: "close")
+          else
+            content            
           end
         end
       end
@@ -87,6 +92,8 @@ module WILL
       #
       # Hash of options.
       #
+      # :id – element id.
+      #
       # :class – will add classes to the tooltip. 
       #
       # :options - Foundation tooltip data-options. (disable_for_touch:true, show_on:large)
@@ -95,8 +102,133 @@ module WILL
       #   <%= f_tooltip "Click Here", "This takes you to a new place", "top", class: "round", options: "disable_for_touch:true" %>
       #
       def f_tooltip(text="", title="", position="bottom", options={})
-        content_tag(:span, data: { tooltip: true, options: options[:options] }, class: "has-tip tip-#{position} #{options[:class]}", title: title) do
+        content_tag(:span, data: { tooltip: true, options: options[:options] }, class: "has-tip tip-#{position} #{options[:class]}", title: title, id: options[:id]) do
           text
+        end
+      end
+
+      ##
+      # Generates a modal. This returns a modal builder that you can use to generate both the modal window and the trigger button.
+      # = Params
+      # +id+
+      #
+      # The id you would like to give the modal. Defaults to will-style-modal
+      #
+      # = Examples
+      #   <%= f_modal "myModal" do |m| %>
+      #     <%= m.trigger "Open my Modal" %>
+      #
+      #     <%= m.modal class="large" do %>
+      #       <strong>My</strong> Modal content
+      #     <% end %>
+      #   <% end %>
+      #
+      def f_modal(id="will-style-modal", &block)
+        raise ArgumentError, "Missing block" unless block_given?
+
+        capture(ModalBuilder.new(id, self), &block)
+      end
+
+      ##
+      # Builder used to generate modal components.
+      #
+      class ModalBuilder
+        ##
+        # The HTML id for the modal window
+        #
+        attr_accessor :id
+
+        ##
+        # The parent template used to access ActionView Helper Methods
+        #
+        attr_accessor :template
+
+        ##
+        # Initialize the ModalBuilder
+        #
+        def initialize(id, template)
+          self.id = id
+          self.template = template
+        end
+
+        delegate :capture, :content_tag, :link_to, to: :template
+
+        ##
+        # Generates a modal trigger. You can either specify button text
+        # or use an ERB Block to fill in HTML content.
+        # = Params
+        # +text+
+        #
+        # text you would like to display in the trigger button.
+        # +options+
+        #
+        # Hash of options.
+        #
+        # :id – element id.
+        #
+        # :class – will add classes to the button.
+        #
+        # +block+
+        #
+        # ERB Block for the trigger button if it needs to be HTML content.
+        #
+        # = Examples
+        # Default Functionality
+        #   <%= m.trigger "Open the modal", class: "round" %>
+        # With ERB Block
+        #   <%= m.trigger do %>
+        #     <h1>Open the modal</h1>
+        #   <% end %>
+        #
+        def trigger(text="", options={}, &block)
+          if block_given?
+            options = options.merge(text) if text.is_a?(Hash)
+            
+            link_to("#", class: "button #{options[:class]}", id: options[:id], data: { "reveal-id" => @id }) do
+              capture(&block)
+            end
+          else
+            link_to(text, "#", class: "button #{options[:class]}", id: options[:id], data: { "reveal-id" => @id })
+          end
+        end
+
+        ##
+        # Generates the modal content.
+        # = Params
+        #
+        # +options+
+        #
+        # Hash of options.
+        #
+        # :id – element id.
+        #
+        # :class – will add classes to the modal. 
+        #
+        # :dismissable - whether or not to include the close button. Defaults to true.
+        #
+        # +block+
+        #
+        # ERB Block for the modal.
+        #
+        # = Examples
+        #   <%= m.modal class="large" do %>
+        #     <strong>My</strong> Modal content
+        #   <% end %>
+        #
+        def modal(options={}, &block)
+          raise ArgumentError, "Missing block" unless block_given?
+
+          content = capture(&block)
+
+          can_dismiss = !options.has_key?(:dismissable) || options[:dismissable] == true
+
+          content_tag(:div, data: { reveal: true }, class: "reveal-modal #{options[:class]}", id: @id, "data-options" => can_dismiss ? "" : "close_on_background_click:false") do
+            if can_dismiss
+              content + @template.link_to("&times;".html_safe, "#", class: "close-reveal-modal")
+            else
+              content              
+            end
+          end
         end
       end
     end
