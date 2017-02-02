664da85d85eaf5b29a6c0ea4e13435d1aab0417c
Dillon Lucente <dillonlucente@gmail.com>
2017-02-02 12:47:09 -0500

    Bumping sassc

--- config/initializers/sassc-rails.rb
+++ config/initializers/sassc-rails.rb
@@ -3,7 +3,7 @@ require "sprockets/engines"
 module Extensions
   module Sprockets
     module Engines
-      def register_engine(ext, klass)
+      def register_engine(ext, klass, options = {})
         if defined?(Sass::Rails)
           return if [
             Sass::Rails::SassTemplate,
@@ -19,10 +19,16 @@ end
 
 Sprockets::Base.send(:prepend, Extensions::Sprockets::Engines)
 
-# Rails.application.assets.register_engine '.sass', SassC::Rails::SassTemplate
-# Rails.application.assets.register_engine '.scss', SassC::Rails::ScssTemplate
-
 Rails.application.config.assets.configure do |env|
-  env.register_engine '.sass', SassC::Rails::SassTemplate
-  env.register_engine '.scss', SassC::Rails::ScssTemplate
+  if env.respond_to?(:register_engine)
+    env.register_engine '.sass', SassC::Rails::SassTemplate, silence_deprecation: true
+    env.register_engine '.scss', SassC::Rails::ScssTemplate, silence_deprecation: true
+  end
+
+  if env.respond_to?(:register_transformer)
+    env.register_transformer 'text/sass', 'text/css',
+      Sprockets::SassProcessor.new(importer: SassC::Rails::Importer, sass_config: Rails.application.config.sass)
+    env.register_transformer 'text/scss', 'text/css',
+      Sprockets::ScssProcessor.new(importer: SassC::Rails::Importer, sass_config: Rails.application.config.sass)
+  end
 end
