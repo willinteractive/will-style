81cde55b7e8c9e1508e89fc79ff35a615a57882c
Dillon Lucente <dlucente@willinteractive.com>
2025-12-19 11:40:24 -0500

    Adding more assets to precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,9 @@
-Rails.application.config.assets.precompile += %w( will-style/email.css )
+Rails.application.config.assets.precompile += [
+   "will-style/email.css",
+
+   "will-style/favicon/apple-touch-icon.png",
+   "will-style/favicon/favicon-32x32.png",
+   "will-style/favicon/favicon-16x16.png",
+   "will-style/favicon/safari-pinned-tab.svg",
+   "will-style/favicon/favicon.ico"
+]
