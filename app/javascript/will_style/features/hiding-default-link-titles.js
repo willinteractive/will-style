5bfbb75a79ef9e1091d8958c63b5299a76fbf255
Dillon Lucente <dlucente@willinteractive.com>
2026-01-27 17:59:31 -0500

    Fixing error on default links

--- app/javascript/will_style/features/hiding-default-link-titles.js
+++ app/javascript/will_style/features/hiding-default-link-titles.js
@@ -4,18 +4,22 @@
   // Remove and Re-Add link titles on links when hovering so we don't get browser pop-in
 
   document.addEventListener("mouseenter", function(event) {
-    const element = event.target.closest("a, button");
-    if (element && element.hasAttribute("title")) {
-      element.setAttribute("data-hover-title", element.getAttribute("title"));
-      element.removeAttribute("title");
+    if (event.target && event.target.closest) {
+      const element = event.target.closest("a, button");
+      if (element && element.hasAttribute("title")) {
+        element.setAttribute("data-hover-title", element.getAttribute("title"));
+        element.removeAttribute("title");
+      }
     }
   }, true);
 
   document.addEventListener("mouseleave", function(event) {
-    const element = event.target.closest("a, button");
-    if (element && element.hasAttribute("data-hover-title")) {
-      element.setAttribute("title", element.getAttribute("data-hover-title"));
-      element.removeAttribute("data-hover-title");
+    if (event.target && event.target.closest) {
+      const element = event.target.closest("a, button");
+      if (element && element.hasAttribute("data-hover-title")) {
+        element.setAttribute("title", element.getAttribute("data-hover-title"));
+        element.removeAttribute("data-hover-title");
+      }
     }
   }, true);
 })();
